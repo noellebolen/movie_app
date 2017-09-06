9242a787b6e064fbee7728ca884bf504c5c47407
Noelle Bolen <nbolen@stitchfix.com>
2017-09-05 20:33:23 -0400

    Updated movie showtimes and styles including grids

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'wdm',      '0.1.1' if Gem.win_platform?
-
+gem 'pgreset'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
  
-  resources :parks
-  resources :movies
-  resources :movie_events
+  
+  get '/movie_events/:id' => 'movie_events#show', as: :movie_event
+	resources :parks, only: [:index, :show]
+		
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'parks#index'
   
